356f1889275fd6b078c318c539d673fad13bed1f
Jose Luis Coloma <colomatormojl@gmail.com>
2024-03-19 00:54:15 +0100

    capitulo 12

--- 12_condicionales_III.py
+++ 12_condicionales_III.py
@@ -2,3 +2,21 @@
 # Concatenacion de operadores de comparacion al utilizar un condicional
 # operadores lógicos and y or
 # operador in
+# Ejemplo Salarios de la empresa
+
+salario_presidente = int(input("Introduce salario Presidente: "))
+print("Salario presidente: " + str(salario_presidente))
+
+salario_director = int(input("Introduce salario del Director: "))
+print(f'Salario directo: {salario_director}')
+
+salario_jefe_area = int(input("Introduce salario del Jefe de Área: "))
+print(f'Salario jefe de área: {salario_jefe_area}')
+
+salario_administrativo = int(input("Introduce salario del Administrativo: "))
+print(f'Administrativo: {salario_administrativo}')
+
+if salario_administrativo < salario_jefe_area < salario_director < salario_presidente:
+    print('Todo funciona correctamente')
+else:
+    print('Algo falla en esta empresa')
